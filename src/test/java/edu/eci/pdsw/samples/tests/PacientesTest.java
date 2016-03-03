55cd50fb898a3169f2ea90d9aaa5ab3e2024062e
Daniela Sepulveda <daniela.sepulveda@mail.escuelaing.edu.co>
2016-03-03 17:08:51 -0500

    Autor 2: Clases de equivalencia

--- src/test/java/edu/eci/pdsw/samples/tests/PacientesTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/PacientesTest.java
@@ -16,6 +16,10 @@
  */
 package edu.eci.pdsw.samples.tests;
 
+import edu.eci.pdsw.samples.entities.Paciente;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
+import edu.eci.pdsw.samples.services.ServiciosPacientesStub;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -25,6 +29,13 @@ import static org.junit.Assert.*;
  * @author hcadavid
  */
 public class PacientesTest {
+    /**
+     * Clases de equivalenci:
+     * 1) Se registra un paciente sin problemas 
+     * 2) Se registra varias veces el mismo paciente
+     * 3) Se registra enviando null, al paciente
+     * 4) 
+     */
     
     public PacientesTest() {
     }
@@ -32,11 +43,32 @@ public class PacientesTest {
     @Before
     public void setUp() {
     }
-    
+    //Metodo RegistrarPacientes
+    //Prueba 1, Se registra sin problemas un paciente
     @Test
-    public void registroPacienteTest(){
-        
+    public void classEqUnoRegistroPacienteSinProblemasTest() {
+        try{
+           Paciente Emma= new Paciente(1,"CC","Emma Caballero",java.sql.Date.valueOf("2013-08-15"));
+           ServiciosPacientesStub servicios= new ServiciosPacientesStub();
+           servicios.registrarNuevoPaciente(Emma);
+           Assert.assertEquals(servicios.consultarPaciente(1,"CC").toString(),Emma.toString());
+        }catch(ExcepcionServiciosPacientes e){
+            Assert.fail("Lanzo excepcion "+ e.getMessage());
+        }
     }
-    
-    
+    //Prueba 2, Se registra el mismo paciente dos veces, deberia presentar excepcion
+    @Test
+    public void classEqDosRegistroPacienteDocumentoIdNegativo(){
+        try{
+           Paciente Thomas= new Paciente(2,"CC","Thomas Caballero",java.sql.Date.valueOf("2010-10-07"));
+           ServiciosPacientesStub servicios= new ServiciosPacientesStub();
+           servicios.registrarNuevoPaciente(Thomas);
+           servicios.registrarNuevoPaciente(Thomas);
+           Assert.fail("No lanzo excepcion");
+        }catch(ExcepcionServiciosPacientes e){
+            Assert.assertTrue("Lanzo Excepcion"+e.getMessage(), true);
+        }
+    }
+  
+       
 }
